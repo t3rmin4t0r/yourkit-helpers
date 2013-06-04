846d492c6205b535f7b2e4ace8e7c1b34d2be885
Gopal V <gopalv@php.net>
2013-06-04 16:16:45 +0530

    PStats adapter for yourkit

--- ourkit/__init__.py
+++ ourkit/__init__.py
@@ -9,6 +9,7 @@ class Call(object):
 		self.time = time
 		self.own_time = own_time
 		self.count = count
+		self.parent = parent
 	def __repr__(self):
 		return "<'%s', time=%d>" % (self.name,self.time)
 
@@ -35,7 +36,7 @@ class CallProfile(object):
 		def intparse(a):
 			x = a.value
 			if(x): return int(x)
-			return 0
+			return 1			
 		time = intparse(node.attributes["time_ms"])
 		own_time = intparse(node.attributes["own_time_ms"])
 		count = intparse(node.attributes["invocation_count"])
@@ -43,6 +44,50 @@ class CallProfile(object):
 		call = Call(parent, name, time, own_time, count)			
 		call.children = [self.parse(child, call) for child in node.childNodes if child.nodeName == "node"]			
 		return call
+
+class PStatsAdapter(object):	
+	def __init__(self, thread):
+		if type(thread) != Call:
+			raise "Wrong type for PStatsAdapter: " + type(thread) + " expected " + Call
+		self.stats = {}
+		self.thread = thread
+	"""
+		{('~', 0, 'func') : (cc, nc, tt, ct, 
+			{"caller" : (cc, nc, tt, ct), }
+		cc = primary calls
+		nc = total calls (cc + recursion)
+		tt = own time
+		ct = time
+	"""
+	def create_stats(self):
+		if(not self.stats):
+			self.finagle(self.thread, set())
+	def finagle(self, call, parents):
+		func = ('~', 0, call.name)
+		# this is why I like python !
+		vadd = lambda a,b: tuple([(i+j) for i,j in zip(list(a), list(b))])
+		if(not self.stats.has_key(func)):
+			self.stats[func] = (0,0,0.0,0.0, {})
+		(cc,nc,tt,ct, callers) = self.stats[func]
+		_cc,_nc,_tt,_ct = (0,0,0.0,0.0)
+
+		if(not (call.name in parents)):
+			_cc = call.count
+			_ct = call.time
+		_nc = call.count
+		_tt = call.own_time
+		old = (cc, nc, tt, ct)		
+		item = (_cc, _nc, _tt, _ct)
+		if(call.parent): 
+			parent = ('~', 0, call.parent.name)
+			if(not callers.has_key(parent)):
+				callers[parent] = (0,0,0.0,0.0)
+			callers[parent] = vadd(callers[parent], item)
+		# yeah, + is tuple concat not vector addition
+		self.stats[func] = vadd(old, item)+(callers,)
+		parents = parents | set([call.name])
+		for child in call.children:	
+			self.finagle(child, parents)
 
 def expensive(root, key=lambda c: c.time):
 	ret = [root]
